f8a3084a6e6fef4778497813b47be5dc750b34e2
vmurali100 <vmurali100@gmail.com>
2020-01-18 18:08:06 +0530

    Adding Modal

--- project2/src/Form.js
+++ project2/src/Form.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useForm } from "./Hook";
-import { Link } from "react-router-dom";
 import axios from "axios";
 
 import { useHistory } from "react-router-dom";
--- project2/src/User.js
+++ project2/src/User.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useReducer } from "react";
 import axios from "axios";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Modal, Form } from "react-bootstrap";
 const reducer = (state, action) => {
   switch (action.type) {
     case "DELTE_USER":
@@ -24,6 +25,35 @@ const defaultState = { users: [] };
 
 export default function User() {
   const [state, dispatch] = useReducer(reducer, defaultState);
+  const [myUser, setMyUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    id: ""
+  });
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    updateUser();
+  });
+  const updateUser = () => {
+    return axios.put("http://localhost:3000/users/" + myUser.id, myUser);
+  };
+  const handleClose = user => {
+    updateUser().then(res => {
+      axios.get("http://localhost:3000/users").then(response => {
+        dispatch({ type: "GET_CONTACTS", payLoad: response.data });
+        setShow(false);
+      });
+    });
+  };
+  const handleShow = user => {
+    setMyUser(user);
+    setShow(true);
+  };
+  const handleChange = e => {
+    setMyUser({ ...myUser, [e.target.name]: e.target.value });
+  };
   const getUsers = type => {
     axios.get("http://localhost:3000/users").then(response => {
       dispatch({ type: type, payLoad: response.data });
@@ -50,14 +80,78 @@ export default function User() {
           </tr>
         </thead>
         <tbody>
-          {state.users.map(user => {
+          {state.users.map((user, index) => {
             return (
-              <tr>
+              <tr key={index}>
                 {Object.values(user).map(val => {
                   return <td>{val}</td>;
                 })}
                 <td>
-                  <button className="btn btn-warning">Edit</button>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      handleShow(user);
+                    }}
+                  >
+                    Edit
+                  </Button>
+                  <Modal show={show} onHide={handleClose} animation={false}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>{myUser.firstName}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Form>
+                        <Form.Group controlId="firstName">
+                          <Form.Label>First Name</Form.Label>
+                          <Form.Control
+                            type="test"
+                            name="firstName"
+                            defaultValue={myUser.firstName}
+                            onChange={e => {
+                              handleChange(e);
+                            }}
+                          />
+                        </Form.Group>
+
+                        <Form.Group controlId="lastName">
+                          <Form.Label>Last Nmae</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="lastName"
+                            defaultValue={myUser.lastName}
+                            onChange={e => {
+                              handleChange(e);
+                            }}
+                          />
+                        </Form.Group>
+
+                        <Form.Group controlId="lastName">
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="email"
+                            defaultValue={myUser.email}
+                            onChange={e => {
+                              handleChange(e);
+                            }}
+                          />
+                        </Form.Group>
+                      </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button
+                        variant="primary"
+                        onClick={() => {
+                          handleClose(user);
+                        }}
+                      >
+                        Save Changes
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
                 </td>
                 <td>
                   <button
